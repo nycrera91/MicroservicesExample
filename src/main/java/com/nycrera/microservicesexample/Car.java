abbcc4b50c67d51702cd8faa57e085ab066a10c3
nycrera <nycrera@gmail.com>
2022-06-28 17:34:37 +0300

    code

--- src/main/java/com/nycrera/microservicesexample/Car.java
+++ src/main/java/com/nycrera/microservicesexample/Car.java
@@ -1,4 +1,11 @@
 package com.nycrera.microservicesexample;
 
 public class Car {
+    //test
+    //test
+    private final int whell;
+
+    public Car(int whell) {
+        this.whell = whell;
+    }
 }
